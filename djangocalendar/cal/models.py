d0dfd22ff030ac085de75e6a743c8bc482c7c897
Olivia <olivia.li@ivadolabs.com>
2021-04-28 11:43:24 -0400

    Add overlap check for calendar events

--- djangocalendar/cal/models.py
+++ djangocalendar/cal/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 
 class Event(models.Model):
@@ -12,3 +13,31 @@ class Event(models.Model):
     def get_html_url(self):
         url = reverse('cal:event_edit', args=(self.id,))
         return f'<a href="{url}"> {self.title} </a>'
+
+    def check_overlap(self, fixed_start, fixed_end, new_start, new_end):
+        overlap = False
+        if new_start == fixed_end or new_end == fixed_start:  # edge case
+            overlap = False
+        elif (new_start >= fixed_start and new_start <= fixed_end) or (
+                new_end >= fixed_start and new_end <= fixed_end):  # innner limits
+            overlap = True
+        elif new_start <= fixed_start and new_end >= fixed_end:  # outter limits
+            overlap = True
+
+        return overlap
+
+    def get_absolute_url(self):
+        url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
+        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
+
+    def clean(self):
+        if self.end_time <= self.start_time:
+            raise ValidationError('Ending times must after starting times')
+
+        events = Event.objects.filter(start_time=self.start_time)
+        if events.exists():
+            for event in events:
+                if self.check_overlap(event.start_time, event.end_time, self.start_time, self.end_time):
+                    raise ValidationError(
+                        'There is an overlap with another event: ' + str(event.start_time)
+                        + '-' + str(event.end_time))
