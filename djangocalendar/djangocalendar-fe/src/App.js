7c43128cedd5ba0da11f594dcc04520c8dcd049c
Olivia <olivia.li@ivadolabs.com>
2021-05-04 11:08:40 -0400

    implement react date time picker

--- djangocalendar/djangocalendar-fe/src/App.js
+++ djangocalendar/djangocalendar-fe/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component, Fragment } from "react";
+import React, {Component, Fragment} from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Calendar from "./components/Calendar";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Header />
-        <Calendar />
-        <Home />
-      </Fragment>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Header/>
+                <Calendar/>
+                <Home/>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
